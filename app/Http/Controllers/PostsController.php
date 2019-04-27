6c1a2f021729a11c4728b8c6f7be698acb89c4b6
Mateo Medus <31256137+mateomedus@users.noreply.github.com>
2019-04-27 13:47:53 -0300

    importar imagenes

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -8,6 +8,16 @@ use App\Post;
 class PostsController extends Controller
 {
     /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => ['index', 'show']]);
+    }
+
+    /**
      * Display a listing of the resource.
      *
      * @return \Illuminate\Http\Response
@@ -38,14 +48,33 @@ class PostsController extends Controller
     {
         $this->validate($request, [
             'title' => 'required',
-            'body' => 'required'
+            'body' => 'required',
+            'cover_image' => 'image|nullable|max:1999'
         ]);
+
+        // Handle file upload
+        if($request->hasFile('cover_image')){
+            // Get filename with the extension
+            $filenameWithExt = $request->file('cover_image')->getClientOriginalName();
+            // Get just filename
+            $filename = pathinfo($filenameWithExt, PATHINFO_FILENAME);
+            //Get just extension
+            $extension = $request->file('cover_image')->getClientOriginalExtension();
+            // Filename to store
+            $filenameToStore = $filename.'_'.time().'.'.$extension;
+            // Upload the image
+            $path = $request->file('cover_image')->storeAs('public/cover_images', $filenameToStore);
+        }
+        else{
+            $filenameToStore = 'noimage.jpg';
+        }
 
         // Create post
         $post = new Post;
         $post->title = $request->input('title');
         $post->body = $request->input('body');
         $post->user_id = auth()->user()->id;
+        $post->cover_image = $filenameToStore;
         $post->save();
         
         return redirect('/posts')->with('success', 'Post Created');
@@ -72,6 +101,10 @@ class PostsController extends Controller
     public function edit($id)
     {
         $post = Post::find($id);
+        //Check for correct user
+        if(auth()->user()->id != $post->user_id){
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
         return view('posts.edit')->with('post',$post);
     }
 
@@ -107,6 +140,10 @@ class PostsController extends Controller
     public function destroy($id)
     {
         $post = Post::find($id);
+        //check for correct user
+        if(auth()->user()->id != $post->user_id){
+            return redirect('/posts')->with('error','Unauthorized Page');
+        }
         $post->delete();
         return redirect('/posts')->with('success', 'Post Removed');
     }
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -2,11 +2,20 @@
 
 @section('content')
     <h1>Posts</h1>
+    <br>
     @if(count($posts) > 0)
         @foreach($posts as $post)
-            <div class="card card-body bg-light">
-                <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
-                <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+            <div class="well">
+                <div class="row">
+                    <div class="col-md-3 col-sm-3">
+                        <img style="width:100%" src="/storage/cover_images/{{$post->cover_image}}">
+                    </div>
+                    <div class="col-md-9 col-sm-9">
+                        <h3><a href="/posts/{{$post->id}}">{{$post->title}}</a></h3>
+                        <small>Written on {{$post->created_at}} by {{$post->user->name}}</small>
+                    </div>
+                </div>
+                
             </div>
         @endforeach
         {{$posts->links()}}
