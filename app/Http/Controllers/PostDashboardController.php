5e447906d85b9d9d828fc5bed5e0a9c7161310f8
Mateo Medus <31256137+mateomedus@users.noreply.github.com>
2019-05-09 13:07:58 -0300

    Update PostDashboardController.php

--- app/Http/Controllers/PostDashboardController.php
+++ app/Http/Controllers/PostDashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Post;
 use App\PostsList;
+use App\User;
 
 class PostDashboardController extends Controller
 {
@@ -27,6 +28,9 @@ class PostDashboardController extends Controller
     {
         $posts = Post::where('postList_id','=',$id)->paginate(10);
         $list = PostsList::find($id);
+        if(auth()->user()->id != $list->user_id){
+            return redirect('/listDashboard')->with('error','Unauthorized Page');
+        }
 
         return view('postDashboard')->with('posts', $posts)->with('list_id',$id)->with('list',$list);
     }
